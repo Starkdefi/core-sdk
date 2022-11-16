e0d5534cd6cb0a20ccf624493444e855045373d9
Iddriss Raaj <idd.iraaj@gmail.com>
2022-11-16 22:24:46 +0000

    feat: add BN fractions

---
+++ src/entities/fractions/fraction.test.ts
@@ -0,0 +1,169 @@
+import BN from 'bn.js';
+import { Fraction } from './fraction';
+
+describe('Fraction', () => {
+  describe('#quotient', () => {
+    it('floor division', () => {
+      expect(new Fraction(new BN(8), new BN(3)).quotient).toEqual(new BN(2)); // one below
+      expect(new Fraction(new BN(12), new BN(4)).quotient).toEqual(new BN(3)); // exact
+      expect(new Fraction(new BN(16), new BN(5)).quotient).toEqual(new BN(3)); // one above
+    });
+  });
+  describe('#remainder', () => {
+    it('returns fraction after divison', () => {
+      expect(new Fraction(new BN(8), new BN(3)).remainder).toEqual(
+        new Fraction(new BN(2), new BN(3))
+      );
+      expect(new Fraction(new BN(12), new BN(4)).remainder).toEqual(
+        new Fraction(new BN(0), new BN(4))
+      );
+      expect(new Fraction(new BN(16), new BN(5)).remainder).toEqual(
+        new Fraction(new BN(1), new BN(5))
+      );
+    });
+  });
+  describe('#invert', () => {
+    it('flips num and denom', () => {
+      expect(new Fraction(new BN(5), new BN(10)).invert().numerator).toEqual(
+        new BN(10)
+      );
+      expect(new Fraction(new BN(5), new BN(10)).invert().denominator).toEqual(
+        new BN(5)
+      );
+    });
+  });
+  describe('#add', () => {
+    it('multiples denoms and adds nums', () => {
+      const sum = new Fraction(new BN(1), new BN(10)).add(
+        new Fraction(new BN(4), new BN(12))
+      );
+      expect(sum.equalTo(new Fraction(new BN(52), new BN(120)))).toBeTruthy();
+    });
+
+    it('same denom', () => {
+      expect(
+        new Fraction(new BN(1), new BN(5)).add(
+          new Fraction(new BN(2), new BN(5))
+        )
+      ).toEqual(new Fraction(new BN(3), new BN(5)));
+    });
+  });
+  describe('#subtract', () => {
+    it('multiples denoms and subtracts nums', () => {
+      expect(
+        new Fraction(new BN(1), new BN(10))
+          .subtract(new Fraction(new BN(4), new BN(12)))
+          .equalTo(new Fraction(new BN(-28), new BN(120)))
+      ).toBeTruthy();
+    });
+    it('same denom', () => {
+      expect(
+        new Fraction(new BN(3), new BN(5)).subtract(
+          new Fraction(new BN(2), new BN(5))
+        )
+      ).toEqual(new Fraction(new BN(1), new BN(5)));
+    });
+  });
+  describe('#lessThan', () => {
+    it('correct', () => {
+      expect(
+        new Fraction(new BN(1), new BN(10)).lessThan(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(true);
+      expect(
+        new Fraction(new BN(1), new BN(3)).lessThan(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(false);
+      expect(
+        new Fraction(new BN(5), new BN(12)).lessThan(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(false);
+    });
+  });
+  describe('#equalTo', () => {
+    it('correct', () => {
+      expect(
+        new Fraction(new BN(1), new BN(10)).equalTo(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(false);
+      expect(
+        new Fraction(new BN(1), new BN(3)).equalTo(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(true);
+      expect(
+        new Fraction(new BN(5), new BN(12)).equalTo(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(false);
+    });
+  });
+  describe('#greaterThan', () => {
+    it('correct', () => {
+      expect(
+        new Fraction(new BN(1), new BN(10)).greaterThan(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(false);
+      expect(
+        new Fraction(new BN(1), new BN(3)).greaterThan(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(false);
+      expect(
+        new Fraction(new BN(5), new BN(12)).greaterThan(
+          new Fraction(new BN(4), new BN(12))
+        )
+      ).toBe(true);
+    });
+  });
+  describe('#multiplty', () => {
+    it('correct', () => {
+      expect(
+        new Fraction(new BN(1), new BN(10))
+          .multiply(new Fraction(new BN(4), new BN(12)))
+          .equalTo(new Fraction(new BN(4), new BN(120)))
+      ).toBeTruthy();
+      expect(
+        new Fraction(new BN(1), new BN(3))
+          .multiply(new Fraction(new BN(4), new BN(12)))
+          .equalTo(new Fraction(new BN(4), new BN(36)))
+      ).toBeTruthy();
+      expect(
+        new Fraction(new BN(5), new BN(12))
+          .multiply(new Fraction(new BN(4), new BN(12)))
+          .equalTo(new Fraction(new BN(20), new BN(144)))
+      ).toBeTruthy();
+    });
+  });
+  describe('#divide', () => {
+    it('correct', () => {
+      expect(
+        new Fraction(new BN(1), new BN(10))
+          .divide(new Fraction(new BN(4), new BN(12)))
+          .equalTo(new Fraction(new BN(12), new BN(40)))
+      ).toBeTruthy();
+      expect(
+        new Fraction(new BN(1), new BN(3)).divide(
+          new Fraction(new BN(4), new BN(12))
+        ).equalTo(new Fraction(new BN(12), new BN(12)))
+      ).toBeTruthy();
+      expect(
+        new Fraction(new BN(5), new BN(12)).divide(
+          new Fraction(new BN(4), new BN(12))
+        ).equalTo(new Fraction(new BN(60), new BN(48)))
+      ).toBeTruthy();
+    });
+  });
+  describe('#asFraction', () => {
+    it('returns an equivalent but not the same reference fraction', () => {
+      const f = new Fraction(1, 2)
+      expect(f.asFraction).toEqual(f)
+      expect(f === f.asFraction).toEqual(false)
+    })
+  })
+});
---
+++ src/entities/fractions/fraction.ts
@@ -0,0 +1,179 @@
+import BN from 'bn.js';
+import invariant from 'tiny-invariant';
+import _Decimal from 'decimal.js-light';
+import _Big from 'big.js';
+import toFormat from 'toformat';
+
+import { BigNumberish, Rounding } from '../../constants';
+
+const Decimal = toFormat(_Decimal);
+const Big = toFormat(_Big);
+
+const toSignificantRounding = {
+  [Rounding.ROUND_DOWN]: Decimal.ROUND_DOWN,
+  [Rounding.ROUND_HALF_UP]: Decimal.ROUND_HALF_UP,
+  [Rounding.ROUND_UP]: Decimal.ROUND_UP,
+};
+
+const toFixedRounding = {
+  [Rounding.ROUND_DOWN]: Big.RoundDown,
+  [Rounding.ROUND_HALF_UP]: Big.RoundHalfUp,
+  [Rounding.ROUND_UP]: Big.RoundUp,
+};
+
+export class Fraction {
+  public readonly numerator: BN;
+  public readonly denominator: BN;
+
+  public constructor(
+    numerator: BigNumberish,
+    denominator: BigNumberish = new BN(1)
+  ) {
+    this.numerator = new BN(numerator);
+    this.denominator = new BN(denominator);
+  }
+
+  private static tryParseFraction(
+    fractionish: BigNumberish | Fraction
+  ): Fraction {
+    if (
+      fractionish instanceof BN ||
+      typeof fractionish === 'number' ||
+      typeof fractionish === 'string'
+    )
+      return new Fraction(fractionish);
+
+    if ('numerator' in fractionish && 'denominator' in fractionish)
+      return fractionish;
+    throw new Error('Could not parse fraction');
+  }
+
+  // performs floor division
+  public get quotient(): BN {
+    return this.numerator.div(this.denominator);
+  }
+
+  // remainder after floor division
+  public get remainder(): Fraction {
+    return new Fraction(this.numerator.mod(this.denominator), this.denominator);
+  }
+
+  public invert(): Fraction {
+    return new Fraction(this.denominator, this.numerator);
+  }
+
+  public add(other: Fraction | BigNumberish): Fraction {
+    const otherParsed = Fraction.tryParseFraction(other);
+    if (this.denominator.eq(otherParsed.denominator)) {
+      return new Fraction(
+        this.numerator.add(otherParsed.numerator),
+        this.denominator
+      );
+    }
+    return new Fraction(
+      this.numerator
+        .mul(otherParsed.denominator)
+        .add(otherParsed.numerator.mul(this.denominator)),
+      this.denominator.mul(otherParsed.denominator)
+    );
+  }
+
+  public subtract(other: Fraction | BigNumberish): Fraction {
+    const otherParsed = Fraction.tryParseFraction(other);
+    if (this.denominator.eq(otherParsed.denominator)) {
+      return new Fraction(
+        this.numerator.sub(otherParsed.numerator),
+        this.denominator
+      );
+    }
+    return new Fraction(
+      this.numerator
+        .mul(otherParsed.denominator)
+        .sub(otherParsed.numerator.mul(this.denominator)),
+      this.denominator.mul(otherParsed.denominator)
+    );
+  }
+
+  public lessThan(other: Fraction | BigNumberish): boolean {
+    const otherParsed = Fraction.tryParseFraction(other);
+    return this.numerator
+      .mul(otherParsed.denominator)
+      .lt(otherParsed.numerator.mul(this.denominator));
+  }
+
+  public equalTo(other: Fraction | BigNumberish): boolean {
+    const otherParsed = Fraction.tryParseFraction(other);
+    return this.numerator
+      .mul(otherParsed.denominator)
+      .eq(otherParsed.numerator.mul(this.denominator));
+  }
+
+  public greaterThan(other: Fraction | BigNumberish): boolean {
+    const otherParsed = Fraction.tryParseFraction(other);
+    return this.numerator
+      .mul(otherParsed.denominator)
+      .gt(otherParsed.numerator.mul(this.denominator));
+  }
+
+  public multiply(other: Fraction | BigNumberish): Fraction {
+    const otherParsed = Fraction.tryParseFraction(other);
+    return new Fraction(
+      this.numerator.mul(otherParsed.numerator),
+      this.denominator.mul(otherParsed.denominator)
+    );
+  }
+
+  public divide(other: Fraction | BigNumberish): Fraction {
+    const otherParsed = Fraction.tryParseFraction(other);
+    return new Fraction(
+      this.numerator.mul(otherParsed.denominator),
+      this.denominator.mul(otherParsed.numerator)
+    );
+  }
+
+  public toSignificant(
+    significantDigits: number,
+    format: object = { groupSeparator: '' },
+    rounding: Rounding = Rounding.ROUND_HALF_UP
+  ): string {
+    invariant(
+      Number.isInteger(significantDigits),
+      `${significantDigits} is not an integer.`
+    );
+    invariant(significantDigits > 0, `${significantDigits} is not positive.`);
+
+    Decimal.set({
+      precision: significantDigits + 1,
+      rounding: toSignificantRounding[rounding],
+    });
+    const quotient = new Decimal(this.numerator.toString())
+      .div(this.denominator.toString())
+      .toSignificantDigits(significantDigits);
+    return quotient.toFormat(quotient.decimalPlaces(), format);
+  }
+
+  public toFixed(
+    decimalPlaces: number,
+    format: object = { groupSeparator: '' },
+    rounding: Rounding = Rounding.ROUND_HALF_UP
+  ): string {
+    invariant(
+      Number.isInteger(decimalPlaces),
+      `${decimalPlaces} is not an integer.`
+    );
+    invariant(decimalPlaces >= 0, `${decimalPlaces} is negative.`);
+
+    Big.DP = decimalPlaces;
+    Big.RM = toFixedRounding[rounding];
+    return new Big(this.numerator.toString())
+      .div(this.denominator.toString())
+      .toFormat(decimalPlaces, format);
+  }
+
+  /**
+   * Helper method for converting any super class back to a fraction
+   */
+  public get asFraction(): Fraction {
+    return new Fraction(this.numerator, this.denominator);
+  }
+}
