b3b2f9c20460b5eceaaf3117ee07cda0b56fce83
Iddriss Raaj <idd.iraaj@gmail.com>
2022-11-16 23:08:55 +0000

    feat: add token class and test

---
+++ src/entities/token.test.ts
@@ -0,0 +1,165 @@
+import { SupportedChainId } from '../constants';
+import { Token } from './token';
+
+describe('Token', () => {
+  const ADDRESS_ONE = '0x0000000000000000000000000000000000000000000000000000000000000001';
+  const ADDRESS_TWO = '0x0000000000000000000000000000000000000000000000000000000000000002';
+
+  describe('#constructor', () => {
+    it('fails with invalid address', () => {
+      expect(
+        () =>
+          new Token(
+            SupportedChainId.GOERLI,
+            '0xhello00000000000000000000000000000000000000000000000000000000002',
+            18
+          ).address
+      ).toThrow(
+        'Invalid character'
+      );
+    });
+    it('fails with negative decimals', () => {
+      expect(
+        () => new Token(SupportedChainId.GOERLI, ADDRESS_ONE, -1).address
+      ).toThrow('DECIMALS');
+    });
+    it('fails with 256 decimals', () => {
+      expect(
+        () => new Token(SupportedChainId.GOERLI, ADDRESS_ONE, 256).address
+      ).toThrow('DECIMALS');
+    });
+    it('fails with non-integer decimals', () => {
+      expect(
+        () => new Token(SupportedChainId.GOERLI, ADDRESS_ONE, 1.5).address
+      ).toThrow('DECIMALS');
+    });
+  });
+
+  describe('#constructor with bypassChecksum = true', () => {
+    const bypassChecksum = true;
+
+    it('creates the token with a valid address', () => {
+      expect(
+        new Token(
+          SupportedChainId.GOERLI,
+          ADDRESS_TWO,
+          18,
+          undefined,
+          undefined,
+          bypassChecksum
+        ).address
+      ).toBe(ADDRESS_TWO);
+    });
+    it('fails with invalid address', () => {
+      expect(
+        () =>
+          new Token(
+            SupportedChainId.GOERLI,
+            '0xhello00000000000000000000000000000000000000000000000000000000002',
+            18,
+            undefined,
+            undefined,
+            bypassChecksum
+          ).address
+      ).toThrow(
+        'Invalid Address Format'
+      );
+    });
+    it('fails with negative decimals', () => {
+      expect(
+        () =>
+          new Token(
+            SupportedChainId.GOERLI,
+            ADDRESS_ONE,
+            -1,
+            undefined,
+            undefined,
+            bypassChecksum
+          ).address
+      ).toThrow('DECIMALS');
+    });
+    it('fails with 256 decimals', () => {
+      expect(
+        () =>
+          new Token(
+            SupportedChainId.GOERLI,
+            ADDRESS_ONE,
+            256,
+            undefined,
+            undefined,
+            bypassChecksum
+          ).address
+      ).toThrow('DECIMALS');
+    });
+    it('fails with non-integer decimals', () => {
+      expect(
+        () =>
+          new Token(
+            SupportedChainId.GOERLI,
+            ADDRESS_ONE,
+            1.5,
+            undefined,
+            undefined,
+            bypassChecksum
+          ).address
+      ).toThrow('DECIMALS');
+    });
+  });
+
+  describe('#equals', () => {
+    it('fails if address differs', () => {
+      expect(
+        new Token(SupportedChainId.GOERLI, ADDRESS_ONE, 18).equals(
+          new Token(SupportedChainId.MAINNET, ADDRESS_TWO, 18)
+        )
+      ).toBe(false);
+    });
+
+    it('false if chain id differs', () => {
+      expect(
+        new Token(SupportedChainId.GOERLI, ADDRESS_ONE, 18).equals(
+          new Token(SupportedChainId.MAINNET, ADDRESS_ONE, 18)
+        )
+      ).toBe(false);
+    });
+
+    it('true if only decimals differs', () => {
+      expect(
+        new Token(SupportedChainId.MAINNET, ADDRESS_ONE, 9).equals(
+          new Token(SupportedChainId.MAINNET, ADDRESS_ONE, 18)
+        )
+      ).toBe(true);
+    });
+
+    it('true if address is the same', () => {
+      expect(
+        new Token(SupportedChainId.MAINNET, ADDRESS_ONE, 18).equals(
+          new Token(SupportedChainId.MAINNET, ADDRESS_ONE, 18)
+        )
+      ).toBe(true);
+    });
+
+    it('true on reference equality', () => {
+      const token = new Token(SupportedChainId.MAINNET, ADDRESS_ONE, 18);
+      expect(token.equals(token)).toBe(true);
+    });
+
+    it('true even if name/symbol/decimals differ', () => {
+      const tokenA = new Token(
+        SupportedChainId.MAINNET,
+        ADDRESS_ONE,
+        9,
+        'abc',
+        'def'
+      );
+      const tokenB = new Token(
+        SupportedChainId.MAINNET,
+        ADDRESS_ONE,
+        18,
+        'ghi',
+        'jkl'
+      );
+      expect(tokenA.equals(tokenB)).toBe(true);
+    });
+  });
+});
