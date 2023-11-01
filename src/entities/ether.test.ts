a1fb08258da5cd8f6c386b7b6113cc0f8d641b46
Iddriss Raaj <idd.iraaj@gmail.com>
2023-11-01 13:33:56 +0000

    update starknetjs

--- src/entities/ether.test.ts
+++ src/entities/ether.test.ts
@@ -2,10 +2,16 @@ import { constants } from 'starknet';
 import { Ether } from './ether';
 
 describe('Ether', () => {
+  // it('static constructor uses cache', () => {
+  //   expect(
+  //     Ether.onChain(constants.StarknetChainId.SN_MAIN) ===
+  //       Ether.onChain(constants.StarknetChainId.SN_MAIN)
+  //   ).toEqual(true);
+  // });
   it('static constructor uses cache', () => {
+    const etherMain = Ether.onChain(constants.StarknetChainId.SN_MAIN);
     expect(
-      Ether.onChain(constants.StarknetChainId.SN_MAIN) ===
-        Ether.onChain(constants.StarknetChainId.SN_MAIN)
+      Number.isNaN(etherMain) ? false : etherMain === Ether.onChain(constants.StarknetChainId.SN_MAIN)
     ).toEqual(true);
   });
   it('caches once per chain ID', () => {
@@ -17,7 +23,7 @@ describe('Ether', () => {
   it('#equals returns false for diff chains', () => {
     expect(
       Ether.onChain(constants.StarknetChainId.SN_MAIN).equals(
-        Ether.onChain(constants.StarknetChainId.SN_GOERLI2)
+        Ether.onChain(constants.StarknetChainId.SN_GOERLI)
       )
     ).toEqual(false);
   });
