85be2b115eb544f87dac1c7398377b829eb2fb26
Lint Action <lint-action@samuelmeuli.com>
2025-08-06 19:54:09 +0000

    Fix code style issues with Prettier

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleNameMapper: {
-    '@starknet-react/chains': '<rootDir>/src/__mocks__/starknet-react-chains.js'
+    '@starknet-react/chains':
+      '<rootDir>/src/__mocks__/starknet-react-chains.js',
   },
   globals: {
     'ts-jest': {
